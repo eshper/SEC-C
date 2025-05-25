f79be4c0d678672bcd53e02aa941bb4ea8922253
Chris <christopherxides@gmail.com>
2025-05-25 09:30:28 +0200

    shaders implemented

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -1,5 +1,36 @@
 #version 330 core
 
+#define MAX_POINT_LIGHTS 8
+
+struct DirectionalLight
+{
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuses;
+    vec3 specular;
+};
+
+struct PointLight
+{
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuses;
+    vec3 specular;
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+uniform sampler2D diffuse0;
+uniform sampler2D specular0;
+uniform sampler2D shadowMap;
+uniform float shine;
+uniform DirectionalLight directionalLight;
+uniform PointLight pointLights[MAX_POINT_LIGHTS];
+uniform int numberOfPointLights;
+uniform vec3 camPos;
+uniform vec3 viewPos;
+
 in vec4 Color;
 in vec3 FragPos;
 in vec3 Normal;
@@ -7,34 +38,135 @@ in vec2 TexCoords;
 
 out vec4 FragColor;
 
-// Material textures
-uniform sampler2D diffuse0;
-uniform sampler2D specular0;
+vec3 calculateDirectionalLightAmbient()
+{
+  return directionalLight.ambient * vec3(texture(material.diffuseMap,textCoord);
+}
 
-// Light
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+vec3 calculatePointLightAmbient(PointLight pointLight)
+{
+  return pointLight.ambient * vec3(texture(diffuse0,textCoord));
+}
+
+vec3 calculateDirectionalLightDiffuse(vec3 lightDirection, vec3 normalizedFragNormal)
+{
+  float diffuseOffset = max(dot(normalizedFragNormal,lightDirection),0.0f);
+
+  return directionalLight.diffuse * diffuseOffset *
+  vec3(texture(diffuse0,textCoord));
+}
+
+vec3 calculatePointLightDiffuse(PointLight pointLight, vec3 lightDirection, vec3 normalizedFragNormal)
+{
+  float diffuseOffset = max(dot(normalizedFragNormal,lightDirection),0.0f);
+
+  return pointLight.diffuse * diffuseOffset *
+  vec3(texture(diffuse0,textCoord));
+}
+
+vec3 calculateDirectionalLightSpecular(vec3 lightDirection, vec3 normalizedFragNormal)
+{
+    vec3 viewDirection = normalize(camPos - fragPos);
+
+    vec3 reflectedLight = reflect(-lightDirection,normalizedFragNormal);
+
+    float specularOffset = pow(max(dot(reflectedLight,viewerDir),0.0f),
+  shine);
+
+  return directionalLight.specular * specularOffset *
+  vec3(texture(specular0,textCoord));
+}
+
+vec3 calculatePointLightSpecular(PointLight pointLight, vec3 lightDirection, vec3 normalizedFragNormal)
+{
+    vec3 viewDirection = normalize(camPos - fragPos);
+
+    vec3 reflectedLight = reflect(-lightDirection,normalizedFragNormal);
+
+    float specularOffset = pow(max(dot(reflectedLight,viewerDir),0.0f),
+  shine);
+
+  return pointLight.specular * specularOffset *
+  vec3(texture(specular0,textCoord));
+}
+
+vec3 calculateDirectionalLightColor(float shadow)
+{
+    vec3 normalizedLightDirection = normalize(-directionalLight.direction);
+    vec3 normalizedFragmentNormal = normalize(Normal);
+
+    vec3 ambientLight  = calculateDirectionalLightAmbient();
+    vec3 diffuseLight  = calculateDirectionalLightDiffuse(normalizedLightDirection,normalizedFragmentNormal);
+    vec3 specularLight = calculateDirectionalLightSpecular(normalizedLightDirection,normalizedFragmentNormal);
+
+    return ambientLight + (1.0 - shadow) * (diffuseLight + specularLight);
+}
+
+vec3 calculatePointLightColor(PointLight pointLight) 
+{
+    vec3 lightDirection = pointLight.position - fragPos;
+    float distance = length(lightDirection);
+    float attenuation = 1.0 / (pointLight.constant + pointLight.linear * distance +
+      pointLight.quadratic * (distance * distance));
+
+    vec3 normalizedLightDirection= normalize(lightDirection);
+    vec3 normalizedFragmentNormal = normalize(Normal);
+
+    vec3 ambientLight  = attenuation * calculatePointLightAmbient();
+    vec3 diffuseLight  = attenuation * calculatePointLightDiffuse(normalizedLightDirection,normalizedFragmentNormal);
+    vec3 specularLight = attenuation * calculatePointLightSpecular(normalizedLightDirection,normalizedFragmentNormal);
+
+    return diffuseLight + ambientLight + specularLight;
+}
+
+float calculateDirectionalShadow(vec4 fragPosLightSpace)
+{
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+
+    projCoords = projCoords * 0.5 + 0.5;
+
+    float closestDepth = texture(shadowMap, projCoords.xy).r; 
+
+    float currentDepth = projCoords.z;
+
+    vec3 normalizedLightDirection = normalize(-directionalLight.direction);
+    vec3 normalizedFragmentNormal = normalize(Normal);
+
+    float bias = max(0.05 * (1.0 - dot(normalizedFragmentNormal, normalizedLightDirection)), 0.005); 
+
+    float shadow = 0.0;
+
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for(int x = -1; x <= 1; x++)
+    {
+        for(int y = -1; y <= 1; y++)
+        {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+            if (currentDepth - bias > pcfDepth) {
+                shadow = 1.0;
+            } 
+        }    
+    }
+    shadow /= 9.0;
+    
+    return shadow;
+}
 
 void main()
 {
-    // Material properties from textures
-    vec3 diffuseColor = texture(diffuse0, TexCoords).rgb;
-    vec3 specularColor = texture(specular0, TexCoords).rgb;
+    vec3 fragmentNormal = normalize(Normal);
+    vec3 viewDirection = normalize(camPos - fragPos);
+    vec3 result = vec3(0.0);
+    
+    float shadow = calculateDirectionalShadow(fragPosLightSpace);
 
-    // Lighting
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
+    result += calculateDirectionalLightColor(shadow); 
 
-    // View direction and reflection
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0); // Shininess is hardcoded here
-
-    vec3 ambient = 0.1 * diffuseColor;
-    vec3 diffuse = diff * diffuseColor;
-    vec3 specular = spec * specularColor;
-
-    vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0);
+    for(int i = 0; i < numPointLights; i++)
+    {
+        result += calculatePointLightColor(pointLights[i], fragmentNormal, fragPos, viewDirection);
+    }
+    
+    fragColor = vec4(result, 1.0);
 }
+
